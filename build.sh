134719b1fd51a91be2cb5d54378347c25f38863b
ddavis <dennis.davis@turner.com>
2016-03-15 14:17:28 -0400

    added group and bumped vesion

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROJECT_NAME=docker-nginx-php
-PROJECT_VERSION=0.0.3
+PROJECT_VERSION=0.0.4
 docker build -t project.build -f Dockerfile.build .
 docker run project.build > build.tar.gz
 docker build -t registry.services.dmtio.net/$PROJECT_NAME:$PROJECT_VERSION -f Dockerfile.dist .
